cd2b59497d5412299a547011d80cb2cf35e96750
Ning Xia <nxiamail@gmail.com>
2015-10-01 17:36:18 -0700

    use recursive http request to account for infinite scroll

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
 /*
-  should account for infinite scroll (AJAX requests)
+  should account for infinite scroll
 */
+
+var Xray = require('x-ray');
+var http = require('http');
+
+var xray = Xray();
+
+var page = 'http://cat.technology/';
+var pageNumber = 2;
+
+var results = [];
+
+// callback for http response chunking
+var callback = function(response){
+  var str='';
+
+  response.on('data', function(chunk){
+    str+=chunk;
+  });
+
+  response.on('end', function(){
+    pageNumber++;
+    // str is a string
+    // check if string contains post, which is the class for content
+    if (str.indexOf('class="post"') > -1){
+      scrape(str)
+    }
+  });
+}
+
+var scrape = function(page){
+  xray(page, '.post',[{
+    src: 'img @src',
+    tags: xray('.has-tags .tag-link', [''])
+  }])
+  (function(err, data){
+    // accumulate results
+    results = results.concat(data);
+
+    // continue to make requests to 'http://cat.technology/page/'+page
+    console.log('this is page number: ', pageNumber);
+
+    // request next page
+    http.get('http://cat.technology/page/'+pageNumber, callback)
+  });
+
+  console.log(results);
+  return results;
+}
+
+// invoke
+scrape(page);
+
+
